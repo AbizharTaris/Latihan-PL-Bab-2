55f94f669db2128aa1d116335586dfcc6ab2f6a6
AbizharTaris <abizhar.kiryuji@gmail.com>
2016-03-09 13:10:18 +0700

    Menambahkan Class Main

---
+++ src/MainLariMahasiswa.java
@@ -0,0 +1,14 @@
+package mainlarimahasiswa;
+import java.util.Scanner;
+public class MainLariMahasiswa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Jumlah Mahasiswa = ");
+        int jumMhs = in.nextInt();
+        int []jum = new int[jumMhs];
+        for (int i = 0; i < jumMhs; i++) {
+            
+        }
+    }
+    
+}
