6cfd7d6ac686e0548da4921532eb285ba04aed3d
AbizharTaris <abizhar.kiryuji@gmail.com>
2016-03-09 14:23:41 +0700

    Update Class Main dan DataMahasiswa

--- src/DataMahasiswa.java
+++ src/DataMahasiswa.java
@@ -9,19 +9,46 @@ public class DataMahasiswa {
     private int banyakKeliling;
     private int waktuKeliling;
     
-    public DataMahasiswa(int banyakMahasiswa){
-        this.banyakMahasiswa = banyakMahasiswa;
+    public DataMahasiswa(){
+        banyakMahasiswa = 0;
+        panjang = 0;
+        lebar = 0;
+        nama = "";
+        nim = "";
+        banyakKeliling = 0;
+        waktuKeliling = 0;
     }
     
-    public DataMahasiswa(int panjang, int lebar){
+    public DataMahasiswa(int banyakMahasiswa ,int panjang, int lebar, String nama, String nim, int banyakKeliling, int waktuKeliling){
+        this.banyakMahasiswa = banyakMahasiswa;
         this.panjang = panjang;
         this.lebar = lebar;
-    }
-    
-    public DataMahasiswa(String nama, String nim, int banyakKeliling, int waktuKeliling){
         this.nama = nama;
         this.nim = nim;
         this.banyakKeliling = banyakKeliling;
         this.waktuKeliling = waktuKeliling;
     }
+    
+    public void setBanyakMahasiswa(int jumMhs){
+        banyakMahasiswa = jumMhs;
+    }
+    public void setPanjang(int p){
+        panjang = p;
+    }
+    public void setLebar(int l){
+        lebar = l;
+    }
+    public void setNama(String n){
+        nama = n;
+    }
+    public void setNIM(String N){
+        nim = N;
+    }
+    public void setBanyakKeliling(int jumKel){
+        banyakKeliling = jumKel;
+    }
+    public void setWaktuKeliling(int wktKel){
+        waktuKeliling = wktKel;
+    }
+    
 }
--- src/MainLariMahasiswa.java
+++ src/MainLariMahasiswa.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class MainLariMahasiswa {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Jumlah Mahasiswa = ");
-        int jumMhs = in.nextInt();
-        int []jum = new int[jumMhs];
-        for (int i = 0; i < jumMhs; i++) {
-            
+        Scanner in2 = new Scanner(System.in);
+        DataMahasiswa mhs = new DataMahasiswa();
+        System.out.print("Jumlah Mahasiswa : ");
+        int jum = in2.nextInt();
+        mhs.setBanyakMahasiswa(jum);
+        System.out.print("Panjang Lapangan : ");
+        int pjg = in2.nextInt();
+        mhs.setPanjang(pjg);
+        System.out.print("Lebar Lapangan : ");
+        int lbr = in2.nextInt();
+        mhs.setLebar(lbr);
+        for (int i = 0; i < jum; i++) {
+            System.out.print("Nama : ");
+            String nama = in.nextLine();
+            System.out.print("NIM : ");
+            String nim = in.nextLine();
         }
     }
     
