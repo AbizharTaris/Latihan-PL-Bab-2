7e15e10d69785d9f580d53106d5a8f4b512de240
AbizharTaris <abizhar.kiryuji@gmail.com>
2016-03-09 13:08:49 +0700

    Penambahan Constructor pada class DataMahasiswa

--- src/DataMahasiswa.java
+++ src/DataMahasiswa.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mainlarimahasiswa;
-
-/**
- *
- * @author asus
- */
 public class DataMahasiswa {
+    private String nama;
+    private String nim;
+    private int panjang;
+    private int lebar;
+    private int kelilingLapangan;
+    private int banyakMahasiswa;
+    private int banyakKeliling;
+    private int waktuKeliling;
     
+    public DataMahasiswa(int banyakMahasiswa){
+        this.banyakMahasiswa = banyakMahasiswa;
+    }
+    
+    public DataMahasiswa(int panjang, int lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    public DataMahasiswa(String nama, String nim, int banyakKeliling, int waktuKeliling){
+        this.nama = nama;
+        this.nim = nim;
+        this.banyakKeliling = banyakKeliling;
+        this.waktuKeliling = waktuKeliling;
+    }
 }
